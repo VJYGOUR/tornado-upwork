4afe391451ebfad6d48baa0469f1fe784a621ffa
vijay kumar <vishaala999111@gmail.com>
2024-07-21 09:25:04 +0530

    commit

--- header.js
+++ header.js
@@ -2,6 +2,131 @@ const burger = document.querySelector(".burger")
 const burgerImg = document.querySelector(".burger img")
 const main = document.querySelector(".main-container")
 const menu = document.querySelector(".mobile-menu")
+var buttons = document.querySelectorAll(".btnn-1");
+var divs = document.querySelectorAll(".cont-btn");
+var allActive = document.querySelectorAll(".all-active");
+var alldivv = document.querySelectorAll(".all-divv");
+var allbtnn = document.querySelectorAll(".all-btnn");
+var btall = document.querySelectorAll(".btall");
+var imgs = document.querySelector(".imgs");
+var allimgs = document.querySelectorAll(".all-imgs");
+var countriesimgs = document.querySelector(".countriesimgs");
+var divimagemain = document.getElementById("divimagemain");
+var x = document.querySelectorAll(".imgs-div");
+
+divimagemain.addEventListener("click", function () {
+  countriesimgs.classList.toggle("hiddenn");
+});
+//
+x.forEach(function (curr, i) {
+  curr.addEventListener("click", function () {
+    imghandler(i);
+  });
+});
+//
+function imghandler(i) {
+  allimgs.forEach(function (imgg, index) {
+    if (i === index) {
+      let myimg = imgg.src;
+      imgs.src = myimg;
+      countriesimgs.classList.add("hiddenn");
+      addcl(index);
+    }
+  });
+}
+// --------------------------------------------------------------------
+// allimgs.forEach(function (imgg, index) {
+//   imgg.addEventListener("click", function () {
+//     let myimg = imgg.src;
+//     imgs.src = myimg;
+//     countriesimgs.style.display = "none";
+//     addcl(index);
+//   });
+// });
+//
+//
+function addcl(index) {
+  x.forEach(function (curr, i) {
+    if (index === i) {
+      curr.classList.add("imgs-background");
+    } else {
+      curr.classList.remove("imgs-background");
+    }
+  });
+}
+//
+//
+allbtnn.forEach(function (button, index) {
+  button.addEventListener("click", function () {
+    showD(index);
+  });
+});
+function showD(indexToShow) {
+  btall.forEach(function (bt, index) {
+    if (index === indexToShow) {
+      bt.classList.add("btall");
+      bt.classList.remove("bttfade");
+    } else {
+      bt.classList.remove("btall");
+      bt.classList.add("bttfade");
+    }
+  });
+}
+allbtnn.forEach(function (button, index) {
+  button.addEventListener("click", function () {
+    showDi(index);
+  });
+});
+
+function showDi(indexToShow) {
+  alldivv.forEach(function (div, index) {
+    if (index === indexToShow) {
+      div.classList.add("hiddenn");
+    } else {
+      div.classList.remove("hiddenn");
+    }
+  });
+}
+buttons.forEach(function (button, index) {
+  console.log(index);
+  button.addEventListener("click", function () {
+    showDiv(index);
+  });
+});
+
+function showDiv(indexToShow) {
+  divs.forEach(function (div, index) {
+    if (index === indexToShow) {
+      div.classList.remove("hiddenn");
+    } else {
+      div.classList.add("hiddenn");
+    }
+  });
+}
+buttons.forEach(function (button, index) {
+  button.addEventListener("click", function () {
+    showDiv(index);
+    // Remove underline from all buttons
+    buttons.forEach(function (button) {
+      button.classList.remove("btnn-1-main");
+    });
+
+    // Add underline to the clicked button
+    button.classList.add("btnn-1-main");
+  });
+});
+allActive.forEach(function (button, index) {
+  button.addEventListener("click", function () {
+    showDiv(index);
+    // Remove underline from all buttons
+    allActive.forEach(function (button) {
+      button.classList.remove("btnn-1-main");
+    });
+    // Add underline to the clicked button
+    button.classList.add("btnn-1-main");
+  });
+});
+
 
 const icons = [`<img src="img/eth.svg">`, `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
 <path
